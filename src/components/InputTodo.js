f59b9be6f3f68ae48f87064d21ba8f71a4b84e34
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-06-11 17:39:39 +0300

    setup todo

---
+++ src/components/InputTodo.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+function InputTodo({ addTodoProps }) {
+  const [title, setTitle] = useState({ title: '' });
+
+  const onChange = (e) => {
+    setTitle({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmitHandle = (event) => {
+    event.preventDefault();
+    if (title.title.trim()) {
+      addTodoProps(title.title);
+      setTitle({ title: '' });
+    } else {
+      alert('Please write item');
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmitHandle}>
+        <input
+          type="text"
+          placeholder="Add todo..."
+          value={title.title}
+          name="title"
+          onChange={onChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+InputTodo.propTypes = {
+  addTodoProps: PropTypes.func.isRequired,
+};
+
+export default InputTodo;
